faff8040de4a9f08c53023ab4012c22c9fe76504
Herlander Bento <herlanderbento19@gmail.com>
2023-03-01 11:09:02 +0100

    Notification Pattern em Products

--- src/domain/product/entity/product-b.ts
+++ src/domain/product/entity/product-b.ts
@@ -1,49 +1,58 @@
-import ProductInterface from './product.interface';
+import Entity from '../../_shared/entity/entity.abstract';
+import NotificationError from '../../_shared/notification/notification.error';
 
-export default class ProductB implements ProductInterface {
-  private _id: string;
+export default class ProductB extends Entity {
   private _name: string;
   private _price: number;
 
   constructor(id: string, name: string, price: number) {
+    super();
     this._id = id;
     this._name = name;
     this._price = price;
     this.validate();
+
+    if (this.notification.hasErrors())
+      throw new NotificationError(this.notification.getErrors());
   }
 
-  get id(): string {
-    return this._id;
-  }
-
-  get name(): string {
+  public get name(): string {
     return this._name;
   }
 
-  get price(): number {
+  public get price(): number {
     return this._price * 2;
   }
 
-  changeName(name: string) {
+  public changeName(name: string) {
     this._name = name;
     this.validate();
   }
 
-  changePrice(price: number) {
+  public changePrice(price: number) {
     this._price = price;
     this.validate();
   }
 
-  validate(): boolean {
+  public validate(): boolean {
     if (this._id.length === 0) {
-      throw new Error('Id is required');
+      this.notification.addError({
+        context: 'product',
+        message: 'Id is required',
+      });
     }
     if (this._name.length === 0) {
-      throw new Error('Name is required');
+      this.notification.addError({
+        context: 'product',
+        message: 'Name is required',
+      });
     }
 
     if (this._price < 0) {
-      throw new Error('Price must be greeter than zero');
+      this.notification.addError({
+        context: 'product',
+        message: 'Price must be greeter than zero',
+      });
     }
 
     return true;
